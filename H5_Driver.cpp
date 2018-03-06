946e461660ad52918474984e590a7b280e010ceb
Damien8x <damien.sudol@gmail.com>
2018-03-06 00:14:40 -0800

    changes to driver
    -ADDED heapsort algorithm to sort Process array; sorted priority by arrival time, secondary by jID value

--- H5_Driver.cpp
+++ H5_Driver.cpp
@@ -11,6 +11,8 @@
 using namespace std;
 
 void welcomeMessage();
+void heapify(Process p[], int n, int i);
+void heapsort(Process p[], int n);
 
 int main()
 {
@@ -19,11 +21,19 @@ string jobID;
 string burstTime;
 string arrivalTime;
 string readFile = "";
-Process p[25];
+int processCount=0;
 
 welcomeMessage();
 
 getline(cin, readFile);
+
+ifstream jobFile;
+jobFile.open(readFile);
+while(getline(jobFile, line))
+	processCount++;
+jobFile.close();
+
+Process p[processCount];
 
 ifstream inFile;
 inFile.open(readFile);
@@ -45,13 +55,22 @@ while (getline(inFile, line)){
 	
 }
 
-for(int i = 0; i < 30; i++){
-	if(p[i].getBT() != -1)
-		cout << p[i].getBT() << endl;
+inFile.close();
+
+
+for(int i = 0; i <processCount ; i++){
+	cout << p[i].getAT() << endl;
 }
 
+heapsort(p, processCount);
 
-inFile.close();
+cout << "************************************************************" << endl;
+
+for(int i = 0; i <processCount ; i++){
+	cout << p[i].getAT() << "	";
+	cout << p[i].getJID() << endl;
+}
+
 return 0;
 }
 
@@ -59,4 +78,39 @@ return 0;
 void welcomeMessage()
 {
 cout << "please enter name process file " << endl;
+}
+
+void heapify(Process p[], int n, int i)
+{
+int largest = i;
+int l = 2*i + 1;
+int r = 2*i + 2;
+
+if(l<n && p[l].getAT() > p[largest].getAT())
+	largest = l;
+if(r<n && p[r].getAT() > p[largest].getAT())
+	largest = r;
+if(r<n && p[r].getAT() == p[largest].getAT() && p[r].getJID() > p[largest].getJID())
+	largest = r;
+if(l<n && p[l].getAT() == p[largest].getAT() && p[l].getJID() > p[largest].getJID())
+	largest = l;
+
+if(largest != i){
+	Process temp = p[largest];
+	p[largest] = p[i];
+	p[i] = temp;
+	heapify(p, n, largest);
+}
+}
+
+void heapsort(Process p[], int n)
+{
+for(int i = n/2 - 1; i >= 0; i --)
+	heapify(p, n, i);
+for(int i = n-1; i>=0; i--){
+	Process temp = p[0];
+	p[0] = p[i];
+	p[i] = temp;
+	heapify(p, i, 0);
+}	
 }
